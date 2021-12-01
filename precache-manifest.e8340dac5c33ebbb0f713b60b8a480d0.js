1bd31bf31a0e91af208e94afc5e92099b7bb8626
Rachel Thiim <rachel.thiim@gmail.com>
2021-12-01 10:07:04 -0600

    Updates

---
+++ precache-manifest.e8340dac5c33ebbb0f713b60b8a480d0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f759c60900073dbdfd935a85b3bef01",
+    "url": "/react-deploy-practice/index.html"
+  },
+  {
+    "revision": "7e3d6701f88f52b560b1",
+    "url": "/react-deploy-practice/static/css/main.434252ff.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/react-deploy-practice/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-deploy-practice/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e3d6701f88f52b560b1",
+    "url": "/react-deploy-practice/static/js/main.bbfae4fc.chunk.js"
+  },
+  {
+    "revision": "87a614c065e73bd8d56d",
+    "url": "/react-deploy-practice/static/js/runtime-main.fe3fcef3.js"
+  },
+  {
+    "revision": "a19f2b67a0540f784bf091db8e36c82a",
+    "url": "/react-deploy-practice/static/media/calculator.a19f2b67.jpg"
+  },
+  {
+    "revision": "402a471b5aad190a33b649f1368f5de1",
+    "url": "/react-deploy-practice/static/media/cover-image.402a471b.jpg"
+  },
+  {
+    "revision": "653c2b3d29f66a3c6e957518f3bd16f0",
+    "url": "/react-deploy-practice/static/media/led-wall.653c2b3d.jpg"
+  },
+  {
+    "revision": "ed494730868bd902e0ee98f1df1877e2",
+    "url": "/react-deploy-practice/static/media/pastel-puzzels.ed494730.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/react-deploy-practice/static/media/run-buddy.938f8fe0.jpg"
+  },
+  {
+    "revision": "12557c9b108cf5e5347f8d838014b31e",
+    "url": "/react-deploy-practice/static/media/surf-report.12557c9b.jpg"
+  }
+]);
